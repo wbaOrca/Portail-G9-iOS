c370174eacc9cb4d00c453d2314edf1793e4586c
WBA_ORCA <WBA_ORCA@MBP-de-macbook.home>
2019-02-08 09:47:40 +0100

    label sous soild gauge

--- iOS/Portail G9/Portail G9/ViewControllers/HomeViewController.swift
+++ iOS/Portail G9/Portail G9/ViewControllers/HomeViewController.swift
@@ -53,29 +53,58 @@ class HomeViewController: UIViewController {
         gaugeView1.areas = "60,40,0,0,0"
         gaugeView1.colorCodes = "00b359,ccffe6,FFFFFF,FFFFFF,FFFFFF"
         gaugeView1.needleValue = 33;
-        
-        let lbl = UILabel(frame: CGRect(x: 0, y: ( gaugeView1.frame.origin.y + 160 ), width: scrollView.frame.size.width, height: 40))
-        lbl.textAlignment = .center //For center alignment
-        lbl.text = "Titre " + String(33)
-        lbl.textColor = .black
-        lbl.backgroundColor = .white//If required
-        lbl.font = UIFont.systemFont(ofSize: 11)
-        lbl.numberOfLines = 0
-        lbl.lineBreakMode = .byWordWrapping
-        scrollView.addSubview(lbl)
-        scrollView.bringSubviewToFront(lbl)
+        let lbl1 = UILabel(frame: CGRect(x: 0, y: ( gaugeView1.frame.origin.y + 160 ), width: scrollView.frame.size.width, height: 40))
+        lbl1.textAlignment = .center //For center alignment
+        lbl1.text = "Titre " + String(33)
+        lbl1.textColor = .black
+        lbl1.backgroundColor = .white//If required
+        lbl1.font = UIFont.systemFont(ofSize: 11)
+        lbl1.numberOfLines = 0
+        lbl1.lineBreakMode = .byWordWrapping
+        scrollView.addSubview(lbl1)
+        scrollView.bringSubviewToFront(lbl1)
         
         gaugeView2.areas = "50,50,0,0,0"
         gaugeView2.colorCodes = "e67300,ffd9b3,FFFFFF,FFFFFF,FFFFFF"
         gaugeView2.needleValue = 45;
+        let lbl2 = UILabel(frame: CGRect(x: 0, y: ( gaugeView2.frame.origin.y + 160 ), width: scrollView.frame.size.width, height: 40))
+        lbl2.textAlignment = .center //For center alignment
+        lbl2.text = "Titre " + String(33)
+        lbl2.textColor = .black
+        lbl2.backgroundColor = .white//If required
+        lbl2.font = UIFont.systemFont(ofSize: 11)
+        lbl2.numberOfLines = 0
+        lbl2.lineBreakMode = .byWordWrapping
+        scrollView.addSubview(lbl2)
+        scrollView.bringSubviewToFront(lbl2)
         
         gaugeView3.areas = "40,60,0,0,0"
         gaugeView3.colorCodes = "ff6666,e6b3b3,FFFFFF,FFFFFF,FFFFFF"
         gaugeView3.needleValue = 73;
+        let lbl3 = UILabel(frame: CGRect(x: 0, y: ( gaugeView3.frame.origin.y + 160 ), width: scrollView.frame.size.width, height: 40))
+        lbl3.textAlignment = .center //For center alignment
+        lbl3.text = "Titre " + String(33)
+        lbl3.textColor = .black
+        lbl3.backgroundColor = .white//If required
+        lbl3.font = UIFont.systemFont(ofSize: 11)
+        lbl3.numberOfLines = 0
+        lbl3.lineBreakMode = .byWordWrapping
+        scrollView.addSubview(lbl3)
+        scrollView.bringSubviewToFront(lbl3)
         
         gaugeView4.areas = "70,30,0,0,0"
         gaugeView4.colorCodes = "003cb3,ccddff,FFFFFF,FFFFFF,FFFFFF"
         gaugeView4.needleValue = 90;
+        let lbl4 = UILabel(frame: CGRect(x: 0, y: ( gaugeView4.frame.origin.y + 160 ), width: scrollView.frame.size.width, height: 40))
+        lbl4.textAlignment = .center //For center alignment
+        lbl4.text = "Titre " + String(33)
+        lbl4.textColor = .black
+        lbl4.backgroundColor = .white//If required
+        lbl4.font = UIFont.systemFont(ofSize: 11)
+        lbl4.numberOfLines = 0
+        lbl4.lineBreakMode = .byWordWrapping
+        scrollView.addSubview(lbl4)
+        scrollView.bringSubviewToFront(lbl4)
     }
     
     // ***********************************
