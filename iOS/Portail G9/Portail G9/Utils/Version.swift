819497c1b6f706d546df268db387419e99d24c6f
WBA_ORCA <WBA_ORCA@MBP-de-macbook.home>
2019-09-06 14:17:37 +0100

    Preparer version PP (Merge R7 PP)

--- iOS/Portail G9/Portail G9/Utils/Version.swift
+++ iOS/Portail G9/Portail G9/Utils/Version.swift
@@ -13,16 +13,23 @@ import UIKit
 // ++++++++++++++++++++++++++++++++++++++++
 // ++++++++++++++++++++++++++++++++++++++++
 class Version: NSObject {
+    
+    //"com.orcaformation.ios.r7.Portail-G9"
+    //"com.orcaformation.ios.pp.Portail-G9"
+    //"com.orcaformation.ios.prod.Portail-G9"
 
-    public static let VERSION = "R7"
+    public static let VERSION = "PP" //"R7"
     
-    public static let URL_PORTAIL_G9 = "https://dpmw-r7-skin.orcaformation.com" //"https://driverg9.dcs2.renault.com" // //
-    public static let URL_WS_PORTAIL_G9 = "https://dpmw-r7-skin.orcaformation.com/api" //"https://driverg9.dcs2.renault.com/api" // //
-    public static let URL_PREFIX_IMAGES_PORTAIL_G9 = "https://dpmw-r7-skin.orcaformation.com/assets/icons/"
+    public static let URL_PORTAIL_G9 = "https://driverg9-pp.orcaformation.com" //"https://dpmw-r7-skin.orcaformation.com" //"https://driverg9.dcs2.renault.com" // //
+    public static let URL_WS_PORTAIL_G9 = "https://driverg9-pp.orcaformation.com/api" //"https://dpmw-r7-skin.orcaformation.com/api" //"https://driverg9.dcs2.renault.com/api" // //
     
-    public static let URL_INSTALL_PORTAIL_G9 = "https://app.orcaformation.com/Renault/G9/r7/driver_g9_install.html"
+    public static let URL_PREFIX_IMAGES_PORTAIL_G9 = "https://driverg9-pp.orcaformation.com/assets/icons/" //"https://dpmw-r7-skin.orcaformation.com/assets/icons/"
     
-    public static let subscriptionTopic = "global_R7" //"global"
+    public static let URL_INSTALL_PORTAIL_G9 = "https://app.orcaformation.com/Renault/G9/pp/driver_g9_install.html" //"https://app.orcaformation.com/Renault/G9/r7/driver_g9_install.html"
+    
+    public static let subscriptionTopic = "global_PP" //"global_R7" //"global_PROD"
+    
+    
     public static let KEY_FCM = "AIzaSyAkGHPHvqJZAzpmBw-ge4b8jH1DyWsPVc4"
     public static let KEY_FCM_LONG = "AAAAGkSdWZM:APA91bEX8CyMshxjrjIP8ENqyt4G4qFkLq0LSfs06pdtxeSZ9UkdXHfFg7AHGTpcRxU4_udA2StVywT3ubjvSnKpdaH3KUCVlcMYoTtLdKkf8RehTkeHQHGu9f0jv9UxcyS0_iGSLGXX"
     
