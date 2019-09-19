9a8a10c2f186549ff411d36171d4cba56fb609f8
WBA_ORCA <WBA_ORCA@MBP-de-macbook.home>
2019-09-19 11:54:32 +0100

    Merge PP Prod 1.0.12

--- iOS/Portail G9/Pods/SDWebImage/SDWebImage/SDWebImageDownloader.m
+++ iOS/Portail G9/Pods/SDWebImage/SDWebImage/SDWebImageDownloader.m
@@ -395,15 +395,21 @@ didReceiveResponse:(NSURLResponse *)response
 
 - (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential *credential))completionHandler {
 
+    completionHandler(NSURLSessionAuthChallengeUseCredential, [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust]);
+    
+    /*
     // Identify the operation that runs this task and pass it the delegate method
     SDWebImageDownloaderOperation *dataOperation = [self operationWithTask:task];
     if ([dataOperation respondsToSelector:@selector(URLSession:task:didReceiveChallenge:completionHandler:)]) {
         [dataOperation URLSession:session task:task didReceiveChallenge:challenge completionHandler:completionHandler];
     } else {
         if (completionHandler) {
-            completionHandler(NSURLSessionAuthChallengePerformDefaultHandling, nil);
+            
+           completionHandler(NSURLSessionAuthChallengePerformDefaultHandling, nil);
         }
-    }
+    
+     */
 }
+
 
 @end
--- iOS/Portail G9/Portail G9/Utils/Version.swift
+++ iOS/Portail G9/Portail G9/Utils/Version.swift
@@ -18,16 +18,16 @@ class Version: NSObject {
     //"com.orcaformation.ios.pp.Portail-G9"
     //"com.orcaformation.ios.prod.Portail-G9"
 
-    public static let VERSION = "PP" //"R7"
+    public static let VERSION = "PROD" //"PP" //"R7"
     
-    public static let URL_PORTAIL_G9 = "https://driverg9-pp.orcaformation.com" //"https://dpmw-r7-skin.orcaformation.com" //"https://driverg9.dcs2.renault.com" // //
-    public static let URL_WS_PORTAIL_G9 = "https://driverg9-pp.orcaformation.com/api" //"https://dpmw-r7-skin.orcaformation.com/api" //"https://driverg9.dcs2.renault.com/api" // //
+    public static let URL_PORTAIL_G9 = "https://driverg9.dcs2.renault.com" //"https://driverg9-pp.orcaformation.com" //"https://dpmw-r7-skin.orcaformation.com" //
+    public static let URL_WS_PORTAIL_G9 = "https://driverg9.dcs2.renault.com/api" //"https://driverg9-pp.orcaformation.com/api" //"https://dpmw-r7-skin.orcaformation.com/api" //
     
-    public static let URL_PREFIX_IMAGES_PORTAIL_G9 = "https://driverg9-pp.orcaformation.com/assets/icons/" //"https://dpmw-r7-skin.orcaformation.com/assets/icons/"
+    public static let URL_PREFIX_IMAGES_PORTAIL_G9 = "https://driverg9.dcs2.renault.com/assets/icons/" //"https://driverg9-pp.orcaformation.com/assets/icons/" //"https://dpmw-r7-skin.orcaformation.com/assets/icons/"
     
-    public static let URL_INSTALL_PORTAIL_G9 = "https://app.orcaformation.com/Renault/G9/pp/driver_g9_install.html" //"https://app.orcaformation.com/Renault/G9/r7/driver_g9_install.html"
+    public static let URL_INSTALL_PORTAIL_G9 = "https://app.orcaformation.com/Renault/G9/prod/driver_g9_install.html" //"https://app.orcaformation.com/Renault/G9/r7/driver_g9_install.html"
     
-    public static let subscriptionTopic = "global_PP" //"global_R7" //"global_PROD"
+    public static let subscriptionTopic = "global_PROD" // "global_PP" //"global_R7" //
     
     
     public static let KEY_FCM = "AIzaSyAkGHPHvqJZAzpmBw-ge4b8jH1DyWsPVc4"
